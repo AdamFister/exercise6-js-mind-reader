df0eb278625254c0367d65d059443a29c21cd080
Adam Fister <arfister@gmail.com>
2019-03-14 20:14:45 -0400

    vuejs implement pages

--- web/js/main.js
+++ web/js/main.js
@@ -1,39 +1,58 @@
-let currentPage = 1;
-
-function goToNextPage() {
-
-    document.getElementById("page " + currentPage).style.display = 'none';
-    currentPage++;
-    document.getElementById("page " + currentPage).style.display = 'block';
-    if (currentPage == 5) {
-        createSymbols();
-    }
-}
-
-function createSymbols() {
-    let str = "";
-    let obj = document.getElementById("symbols");
-    let arrSymbol = shuffle(["&", "!", "@", "#", "$", "%", "^", "*", "?"]);
-
-    let k =0;
-    for (let i = 0; i < 11; i++) {
-
-        for (let j = 0; j < arrSymbol.length; j++) {
-            str+= "<div class='row'>" + k + "-" + arrSymbol[j] + "</div>";
-            k++;
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        pages: [
+            {
+                pageId: 1,
+                pageTitle: 'I can read your mind',
+                pageP1: '',
+                pageP2: ''
+            },
+            {
+                pageId: 2,
+                pageTitle: 'Pick a number from 01 - 99',
+                pageP1: 'When you have your number click next',
+                pageP2: ''
+            },
+            {
+                pageId: 3,
+                pageTitle: 'Add both digits together to get a new number',
+                pageP1: 'Ex: 14 is 1 + 4 = 5',
+                pageP2: 'Click next to proceed'
+            },
+            {
+                pageId: 4,
+                pageTitle: 'Subtract your new number from the original number',
+                pageP1: 'Ex: 14 - 5 = 9',
+                pageP2: 'Click next to proceed'
+            },
+            {
+                pageId: 5,
+                pageTitle: 'I can read your mind',
+                pageP1: 'Find your new number',
+                pageP2: 'Note the symbol beside the number'
+            },
+            {
+                pageId: 6,
+                pageTitle: '',
+                pageP1: '',
+                pageP2: ''
+            }
+        ],
+        currentPage: 0,
+        disableNext: false,
+    },
+    methods: {
+        goToNextPage() {
+            this.currentPage++;
+            
+            if (this.currentPage == 6) {
+                console.log(this.disableNext);
+                this.disableNext = true;
+                console.log(this.disableNext);
+                //return this.disableNext;
+            }
         }
     }
-    str+= "<div class='row'>" + k + "-" + arrSymbol[0] + "</div>";
-    obj.innerHTML = str;
-    document.getElementById('answer').innerHTML = arrSymbol[0];
-}
-
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      let temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-    return array;
-  }
+})
