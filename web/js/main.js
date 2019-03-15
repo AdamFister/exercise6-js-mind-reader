db6bea322db0c26a1c55d7cf002e087ec91700ba
Adam Fister <arfister@gmail.com>
2019-03-15 08:23:15 -0400

    finish

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
+        message: 'I can read your mnind!',
+        symbols: '',
+        answer: '',
+        answerText: '',
         pages: [
             {
                 pageId: 1,
@@ -29,63 +32,69 @@ var app = new Vue({
             },
             {
                 pageId: 5,
-                pageTitle: 'I can read your mind',
+                pageTitle: '',
                 pageP1: 'Find your new number',
                 pageP2: 'Note the symbol beside the number'
             },
             {
                 pageId: 6,
-                pageTitle: '',
-                pageP1: '',
+                pageTitle: this.answer,
+                pageP1: this.answerText,
                 pageP2: ''
             }
         ],
         currentPage: 0,
         disableNext: false,
-        arrSymbol: ["&", "^", "%", "$", "#", "@", "!", "*", "?"]
-
+        showSymbols: false,
+        showAnswer: false,
     },
     methods: {
         goToNextPage() {
             this.currentPage++;
             console.log(this.currentPage);
-            // if (this.currentPage == 4) {
-            //     app.createSymbols();
-            //     //console.log(arrSymbol);
-            // }
+            if (this.currentPage == 4) {
+                app.createSymbols();
+            }
             if (this.currentPage == 5) {
                 this.disableNext = true;
+                this.showSymbols = false;
+                this.showAnswer = true;
             }
         },
+        createSymbols() {
+            let arrSymbol = app.shuffleArray(["&", "^", "%", "$", "#", "@", "!", "*", "?"]);
+            var str = "";
+            console.log(arrSymbol);
 
-        shuffledArray: function () {
-            for (var i = this.arrSymbol.length - 1; i > 0; i--) {
-                var j = Math.floor(Math.random() * (i + 1));
-                var temp = this.arrSymbol[i];
-                this.arrSymbol[i] = this.arrSymbol[j];
-                this.arrSymbol[j] = temp;
-                return this.arrSymbol;
-                // var str = "";
-                // var obj = document.getElementById("symbolsHere");
+            var k = 0;
+            for (let i = 0; i <= 10; i++) {
+                for (let j = 0; j < arrSymbol.length; j++) {
 
-
-                // var k = 0;
-                // for (let i = 0; i <= 10; i++) {
-                //     for (let j = 0; j < arrSymbol.length; j++) {
-
-                //         str += "<div class='row'>" + k + " - " + arrSymbol[j] + "</div>";
-                //         k++;
-                //     }
-                // }
-                // str += "<div class='row'>" + k + " - " + arrSymbol[0] + "</div>";
-
-                // console.log(str);
-                // obj.innerHTML = str;
-
-                // document.getElementById('answer').innerHTML = arrSymbol[0];
-                // document.getElementById('answerText').innerHTML = "Your symbol is " + arrSymbol[0];
-
-
+                    str += "<div class='row'>" + k + " - " + arrSymbol[j] + "</div>";
+                    k++;
+                }
             }
-        
-    }}})
+            str += "<div class='row'>" + k + " - " + arrSymbol[0] + "</div>";
+
+            this.symbols = str;
+            this.showSymbols = true;
+            this.answer = arrSymbol[0];
+            this.answerText = "Your symbol is " + arrSymbol[0];
+            
+            //console.log(this.symbols);
+            //console.log(this.symbols[0]);
+            //console.log(this.answer);
+            //console.log(this.answerText);
+            
+        },
+        shuffleArray(array) {
+            for (var i = array.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            return array;
+        }
+    }
+})
